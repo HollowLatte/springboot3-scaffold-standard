b0942991889dbc80710890114bddc52ac48fc611
hollowlatte <hollowlatte@outlook.com>
2025-09-17 23:43:41 +0800

    refactor: update exclude autoconfigure

--- ddd-scaffold-standard/src/main/resources/archetype-resources/start/src/main/java/Application.java
+++ ddd-scaffold-standard/src/main/resources/archetype-resources/start/src/main/java/Application.java
@@ -8,11 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(scanBasePackages = {"${package}"},
-        exclude = {
-                DataSourceAutoConfiguration.class
-        }
-)
+@SpringBootApplication(scanBasePackages = {"${package}"})
 public class Application {
 
     public static void main(String[] args) {
