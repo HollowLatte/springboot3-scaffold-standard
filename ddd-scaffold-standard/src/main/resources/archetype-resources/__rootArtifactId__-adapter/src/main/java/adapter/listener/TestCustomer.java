1c920e4b5824b22e0b4c6980f82a590077b81499
hollowlatte <hollowlatte@outlook.com>
2025-12-24 18:14:18 +0800

    feat(amqp): Integrate RabbitMQ

---
+++ ddd-scaffold-standard/src/main/resources/archetype-resources/__rootArtifactId__-adapter/src/main/java/adapter/listener/TestCustomer.java
@@ -0,0 +1,17 @@
+package ${package}.adapter.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TestCustomer {
+
+    @RabbitListener(queuesToDeclare = {@Queue("test.queue")})
+    public void receive(String message) {
+        log.info("Received message: {}", message);
+    }
+
+}
