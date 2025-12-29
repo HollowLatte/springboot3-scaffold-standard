bb2afb33b4edbc5619583629cd031bbb199fa70d
hollowlatte <hollowlatte@outlook.com>
2025-12-29 18:18:18 +0800

    refactor(adapter): comment RabbitListener

--- ddd-scaffold-standard/src/main/resources/archetype-resources/__rootArtifactId__-adapter/src/main/java/adapter/listener/TestCustomer.java
+++ ddd-scaffold-standard/src/main/resources/archetype-resources/__rootArtifactId__-adapter/src/main/java/adapter/listener/TestCustomer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestCustomer {
 
-    @RabbitListener(queuesToDeclare = {@Queue("test.queue")})
+    // @RabbitListener(queuesToDeclare = {@Queue("test.queue")})
     public void receive(String message) {
         log.info("Received message: {}", message);
     }
